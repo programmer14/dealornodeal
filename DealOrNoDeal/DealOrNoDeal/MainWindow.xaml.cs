d30597d853485006900eb359d8f21790e5f910d9
Janis <Janis@JANIS-HP>
2018-03-01 16:12:41 +0100

    Event koffer verschwindelah

--- DealOrNoDeal/DealOrNoDeal/MainWindow.xaml.cs
+++ DealOrNoDeal/DealOrNoDeal/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,25 @@ namespace DealOrNoDeal
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var bild = (Image)sender;
+            bild.Visibility = Visibility.Hidden;
+        }
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
